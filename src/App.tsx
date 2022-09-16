959ed5ac30031974a7c3cb49d1ccd3f6047979fa
Tommy Svensson <4046409+krokben@users.noreply.github.com>
2022-09-16 12:57:20 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, ReactNode } from "react";
+import "./App.css";
+
+enum Status {
+  Idle,
+  Fetching,
+  Success,
+  Error,
+}
+
+enum ErrorMessage {
+  Unauthorized,
+  Server,
+}
+
+const errorMessage: Record<ErrorMessage, string> = {
+  [ErrorMessage.Unauthorized]: "Couldn't fetch because not authorized.",
+  [ErrorMessage.Server]: "Server error occurred.",
+};
+
+const fetchResources = async () => {
+  try {
+    const response = await fetch("https://swapi.dev/api");
+    const resources = await response.json();
+
+    if (response.status !== 200) {
+      return {
+        fetchedStatus: Status.Error,
+        fetchedError: ErrorMessage.Unauthorized,
+      };
+    }
+
+    return {
+      fetchedResources: Object.keys(resources),
+      fetchedStatus: Status.Success,
+    };
+  } catch (error) {
+    console.error(error);
+    return { fetchedStatus: Status.Error, fetchedError: ErrorMessage.Server };
+  }
+};
+
+const StatusBoundary = ({
+  children,
+  status,
+  error,
+}: {
+  children: ReactNode;
+  status: Status;
+  error: ErrorMessage;
+}) =>
+  status === Status.Idle ? (
+    <></>
+  ) : status === Status.Fetching ? (
+    <p>Fetching data...</p>
+  ) : status === Status.Error ? (
+    <p>{errorMessage[error]}</p>
+  ) : (
+    <>{children}</>
+  );
 
 function App() {
+  const [status, setStatus] = useState<Status>(Status.Idle);
+  const [error, setError] = useState<ErrorMessage>(ErrorMessage.Server);
+  const [resources, setResources] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchAndSetResources = async () => {
+      const { fetchedResources, fetchedStatus, fetchedError } =
+        await fetchResources();
+
+      if (fetchedError) {
+        setError(fetchedError);
+        setStatus(fetchedStatus);
+        return;
+      }
+
+      if (fetchedResources) {
+        setResources(fetchedResources);
+        setStatus(fetchedStatus);
+      }
+    };
+    fetchAndSetResources();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Star Wars</h1>
+      <StatusBoundary status={status} error={error}>
+        <ul>
+          {resources.map((resource) => (
+            <li key={resource}>{resource}</li>
+          ))}
+        </ul>
+      </StatusBoundary>
     </div>
   );
 }
